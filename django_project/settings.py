4857813ba13d3ed752e1a07836b2e2b431a2b0a8
Joshua Ayanlowo <joshuaayanlowo235@gmail.com>
2024-05-26 04:31:02 +0100

    minor inconsequencial adjustments

--- django_project/settings.py
+++ django_project/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'articles.apps.ArticlesConfig',
 ]
 
-AUTH_USER_MODEL = 'accounts.CustomUser'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
